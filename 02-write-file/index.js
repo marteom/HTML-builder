1b683534a65a78c87145d6e5830aa8e0987f47d2
marteom <marteom@gmail.com>
2022-11-05 23:07:38 +0300

    feat: add code for task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,3 +1,29 @@
-const readline = require('readline/promises');
+const readline = require('readline');
 const fs = require('fs');
 const path = require('node:path');
+const { stdin: input, stdout: output } = require('process');
+
+outputStream = fs.createWriteStream(path.join(__dirname, 'text.txt'), {flags: 'a'});
+
+const rl = readline.createInterface({ input, output });
+
+output.write('enter your text: ');
+
+rl.on('line', (input) => {
+  if (input === 'exit') 
+  {
+    doneInput(output);
+    return;
+  }
+  outputStream.write(input);
+});
+
+rl.on('SIGINT', () => {
+  doneInput(output);
+});
+
+const doneInput = (output) => {
+  rl.close();
+  output.write('Goodbye!');
+  outputStream.end();
+}
