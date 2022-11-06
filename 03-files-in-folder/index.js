e73b3d288e33916524e6a0c59693d653c8dfaefb
marteom <marteom@gmail.com>
2022-11-07 01:25:25 +0300

    feat: displayed required files data

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,11 +1,25 @@
 const { readdir } = require('node:fs/promises');
-const fs = require('fs');
+const { stat } = require('fs');
 const path = require('node:path');
 
 const dirPath = path.join(__dirname, 'secret-folder');
 
-const folderObjs = readdir(dirPath, {withFileTypes: true});
-folderObjs.then((objData) => {
-  for (const obj of objData)
-  console.log(obj.name, obj.isFile());
-})
+const showFilesInfo = (dirPath) => {
+  const folderObjs = readdir(dirPath, {withFileTypes: true});
+  folderObjs.then((objData) => {
+    for (const obj of objData) {
+      if(obj.isFile()) {
+        stat(path.join(dirPath, obj.name), (err, stats) => {
+          if (err) throw Error(err.message);
+          let result = path.parse(obj.name).name + ' - ' + path.extname(path.join(dirPath, obj.name)).substring(1) + ' - ' + `${(stats.size/1024).toFixed(3)}kb`;
+          console.log(result);
+        });
+      }
+      else if(obj.isDirectory()){
+        showFilesInfo(path.join(dirPath, obj.name));
+      }
+    }
+  })
+}
+
+showFilesInfo(dirPath);
