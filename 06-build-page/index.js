50005221134010c19e1d2614a9e864c5c78f0723
marteom <marteom@gmail.com>
2022-11-07 17:22:22 +0300

    feat: create dist directory

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,1 +1,10 @@
 const path = require('node:path');
+const { mkdir, copyFile } = require('node:fs');
+
+const distDir = path.join(__dirname, 'project-dist');
+
+mkdir(distDir, { recursive: true }, (err) => {
+    if (err) throw err;
+
+    
+});
