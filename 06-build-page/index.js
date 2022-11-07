07dcdcf849751712285f19087b71f049c444bddd
marteom <marteom@gmail.com>
2022-11-07 17:26:11 +0300

    refactor: change callback mkdir on promises

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,10 +1,11 @@
 const path = require('node:path');
-const { mkdir, copyFile } = require('node:fs');
+const { mkdir, copyFile } = require('node:fs/promises');
 
 const distDir = path.join(__dirname, 'project-dist');
 
-mkdir(distDir, { recursive: true }, (err) => {
-    if (err) throw err;
 
-    
-});
+const createDistDir = async (distDir) => {
+    await mkdir(distDir, { recursive: true });
+}
+
+createDistDir(distDir);
