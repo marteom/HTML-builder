6be0c9887eb7a0087c273b50b5309cf3be75f876
marteom <marteom@gmail.com>
2022-11-07 17:36:01 +0300

    feat: read template file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,11 +1,17 @@
 const path = require('node:path');
 const { mkdir, copyFile } = require('node:fs/promises');
 
-const distDir = path.join(__dirname, 'project-dist');
+const fsPromises = require('fs/promises');
 
+const distDir = path.join(__dirname, 'project-dist');
 
 const createDistDir = async (distDir) => {
     await mkdir(distDir, { recursive: true });
+
+    const templateData = await fsPromises.readFile(path.join(__dirname, 'template.html'), { encoding: 'utf8' });       
+    //console.log(templateData.toString());
+
+
 }
 
 createDistDir(distDir);
