510180c0cd716f33888510ebb6ca0645281998c0
marteom <marteom@gmail.com>
2022-11-07 13:25:47 +0300

    feat: create file-copy dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,2 +1,8 @@
 
-import { mkdir } from 'node:fs';
+const { mkdir, copyFile } = require('node:fs');
+const path = require('node:path');
+
+
+mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) throw Error(err.message);
+  });
