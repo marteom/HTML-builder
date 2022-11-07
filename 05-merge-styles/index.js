64a08a9f22164aafd87757b3c9522ed0c9239fd8
marteom <marteom@gmail.com>
2022-11-07 14:52:37 +0300

    feat: read files

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,13 +1,19 @@
 const path = require('node:path');
 const { readdir } = require('node:fs/promises');
+const fs = require('fs');
 
 const stylesDir = path.join(__dirname, 'styles');
+
+let filesArray = [];
 
 const folderObjs = readdir(stylesDir, {withFileTypes: true});
 folderObjs.then((objData) => {
     for (const obj of objData) {                
         if(obj.isFile() && path.extname(path.join(stylesDir, obj.name)).substring(1).toLowerCase() === 'css') {
-            console.log(obj.name);
+            const data = fs.readFileSync(path.join(stylesDir, obj.name)); //, function(err, data) {          
+            filesArray.push(data.toString());
         }
     }
+
+    console.log('filesArray.length: ',filesArray.length);
 });
