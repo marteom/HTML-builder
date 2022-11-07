18b12411ac37844fb713ef932384ca6702163df7
marteom <marteom@gmail.com>
2022-11-07 14:58:16 +0300

    feat: create empty bundle.css

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,6 +3,7 @@ const { readdir } = require('node:fs/promises');
 const fs = require('fs');
 
 const stylesDir = path.join(__dirname, 'styles');
+const bundleDir = path.join(__dirname, 'project-dist');
 
 let filesArray = [];
 
@@ -10,10 +11,14 @@ const folderObjs = readdir(stylesDir, {withFileTypes: true});
 folderObjs.then((objData) => {
     for (const obj of objData) {                
         if(obj.isFile() && path.extname(path.join(stylesDir, obj.name)).substring(1).toLowerCase() === 'css') {
-            const data = fs.readFileSync(path.join(stylesDir, obj.name)); //, function(err, data) {          
+            const data = fs.readFileSync(path.join(stylesDir, obj.name));        
             filesArray.push(data.toString());
         }
     }
 
     console.log('filesArray.length: ',filesArray.length);
+
+
+    outputStream = fs.createWriteStream(path.join(bundleDir, 'bundle.css'), {flags: 'a'});
+    outputStream.write('');
 });
