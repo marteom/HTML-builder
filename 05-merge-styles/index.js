0a0b1d484e3d415482bda28aad2c62b2f1127f99
marteom <marteom@gmail.com>
2022-11-07 14:13:31 +0300

    feat: get css files

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,2 +1,13 @@
 const path = require('node:path');
 const { readdir } = require('node:fs/promises');
+
+const stylesDir = path.join(__dirname, 'styles');
+
+const folderObjs = readdir(stylesDir, {withFileTypes: true});
+folderObjs.then((objData) => {
+    for (const obj of objData) {                
+        if(obj.isFile() && path.extname(path.join(stylesDir, obj.name)).substring(1).toLowerCase() === 'css') {
+            console.log(obj.name);
+        }
+    }
+});
