b38f55b5c17f8db338e79851b232cc8cf67ebc2d
marteom <marteom@gmail.com>
2022-11-07 18:09:44 +0300

    refactor: diff code on any functions

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,11 +3,7 @@ const { mkdir, copyFile } = require('node:fs/promises');
 
 const fsPromises = require('fs/promises');
 
-const distDir = path.join(__dirname, 'project-dist');
-
-const createDistDir = async (distDir) => {
-    await mkdir(distDir, { recursive: true });
-
+const createFilledTemplate = async (distDir) => {
     const templateData = await fsPromises.readFile(path.join(__dirname, 'template.html'), { encoding: 'utf8' });    
     
     const headerComponent = await fsPromises.readFile(path.join(__dirname, 'components', 'header.html'), { encoding: 'utf8' }); 
@@ -16,8 +12,18 @@ const createDistDir = async (distDir) => {
 
     const filledTemplate = templateData.replace('{{header}}', headerComponent).replace('{{articles}}', articlesComponent).replace('{{footer}}', footerComponent);
 
-    await fsPromises.writeFile(path.join(distDir, 'index.html'), filledTemplate)
-
+    await fsPromises.writeFile(path.join(distDir, 'index.html'), filledTemplate);
 }
 
-createDistDir(distDir);
+const createDistDir = async () => {
+    const distDirPath = path.join(__dirname, 'project-dist');
+    await mkdir(distDirPath, { recursive: true });
+
+    return distDirPath;
+}
+
+const exec = async () => {
+    const distDir = await createDistDir();
+    await createFilledTemplate(distDir);
+};
+exec();
