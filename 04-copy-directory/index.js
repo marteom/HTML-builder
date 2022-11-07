cbb6b72f39c028df6f64b2517b27579c98040827
marteom <marteom@gmail.com>
2022-11-07 13:57:59 +0300

    feat: copy files and folders

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,31 @@
 
 const { mkdir, copyFile } = require('node:fs');
 const path = require('node:path');
+const { readdir } = require('node:fs/promises');
 
+const sourceDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
 
-mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
-    if (err) throw Error(err.message);
-  });
+const copyDir = (sourceDir, destDir) => {
+    mkdir(destDir, { recursive: true }, (err) => {
+        if (err) throw err;
+        const folderObjs = readdir(sourceDir, {withFileTypes: true});
+        folderObjs.then((objData) => {
+        for (const obj of objData) {                
+            if(obj.isFile()) {
+                copyFile(path.join(sourceDir, obj.name), path.join(destDir, obj.name), (err) => {
+                    if (err) throw err;
+                });
+            }
+            else if(obj.isDirectory()){
+                mkdir(path.join(destDir, obj.name), { recursive: true }, (err) => {
+                    if (err) throw err;
+                    copyDir(path.join(sourceDir, obj.name), path.join(destDir, obj.name));
+                });
+            }
+        }
+        });
+    })
+}
+
+copyDir(sourceDir, destDir);
