734fa936e988cb986ece484bbd524fc1e6deb83b
marteom <marteom@gmail.com>
2022-11-07 17:40:20 +0300

    feat: read components files

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -8,9 +8,13 @@ const distDir = path.join(__dirname, 'project-dist');
 const createDistDir = async (distDir) => {
     await mkdir(distDir, { recursive: true });
 
-    const templateData = await fsPromises.readFile(path.join(__dirname, 'template.html'), { encoding: 'utf8' });       
-    //console.log(templateData.toString());
+    const templateData = await fsPromises.readFile(path.join(__dirname, 'template.html'), { encoding: 'utf8' });    
+    
+    const headerComponent = await fsPromises.readFile(path.join(__dirname, 'components', 'header.html'), { encoding: 'utf8' }); 
+    const articlesComponent = await fsPromises.readFile(path.join(__dirname, 'components', 'articles.html'), { encoding: 'utf8' }); 
+    const footerComponent = await fsPromises.readFile(path.join(__dirname, 'components', 'footer.html'), { encoding: 'utf8' }); 
 
+    //console.log(footerComponent.toString());
 
 }
 
